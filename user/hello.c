515e10b49844b1cf06ebeafb0a8619bb211e9d7d
Jon Rice <ricej13@unlv.nevada.edu>
2025-09-11 18:06:00 -0700

    My Solution for Project 1 on xv6

---
+++ user/hello.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(){
+            printf("Hello world, I am Jon Rice.\n");
+            printf("I am in CS370, Fall 2025, section 1003.\n\n");
+
+            exit(0);
+}
